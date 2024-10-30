885050709be0fc3183956d91fb4ff25d6b6ed818
Alex Grachev <alex.f.grach@gmail.com>
2024-10-30 16:14:11 +0300

    Worker moved to own file

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,152 +1,51 @@
 package app
 
-import org.apache.pekko.actor.typed.ActorSystem
-import org.apache.pekko.actor.typed.scaladsl.Behaviors
-
-import org.apache.pekko.http.scaladsl.client
-import java.net.URI
-import java.io.IOException
-import org.apache.pekko.actor.typed.ActorRef
-import scala.concurrent.Future
-import org.apache.pekko.http.scaladsl.model.HttpResponse
+import org.apache.pekko.actor.typed._
+import org.apache.pekko.actor.typed.scaladsl._
 import org.apache.pekko.http.scaladsl.Http
-import org.apache.pekko.http.scaladsl.model.HttpRequest
-import org.apache.pekko.http.scaladsl.model.Uri
-import scala.util.Failure
-import org.apache.pekko.actor.typed.scaladsl.StashBuffer
-import app.Worker._
-import org.apache.pekko.actor.typed.scaladsl.StashBuffer
-import org.apache.pekko.actor.typed.scaladsl.ActorContext
-import org.apache.pekko.actor.typed.Behavior
-import scala.util.Success
-import org.apache.pekko.http.scaladsl.model.StatusCodes
-import scala.concurrent.ExecutionContext
-import org.apache.pekko.http.scaladsl.model.ResponseEntity
+import org.apache.pekko.http.scaladsl.model._
+import org.apache.pekko.http.scaladsl.server.Directives._
 import org.apache.pekko.util.ByteString
+
+import java.io.IOException
+import java.net.URI
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.io.StdIn
+import scala.util._
 import scala.util.matching.Regex
 
-object Main extends App {
+object Main {
 
   val STASH_SIZE = 100
 
-}
+  def main(args: Array[String]): Unit = {
 
-class Worker(ctx: ActorContext[Command], stashBuffer: StashBuffer[Command])(
-    implicit ec: ExecutionContext
-) {
+    implicit val system = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.executionContext
 
-  implicit val system = ctx.system
-
-  def ready(): Behavior[Command] =
-    Behaviors.receiveMessagePartial[Command] {
-
-      case Command.DoRequest(uri, replyTo) =>
-        ctx.pipeToSelf(doRequest(uri, replyTo)) {
-          case Failure(exception) =>
-            Command.DoAnswer(
-              Event.Err.general(
-                uri,
-                message = s"An error occurred[${exception.getMessage()}]"
-              ),
-              replyTo
+    val route =
+      path("hello") {
+        get {
+          complete(
+            HttpEntity(
+              ContentTypes.`text/html(UTF-8)`,
+              "<h1>Say hello to Pekko HTTP</h1>"
             )
-          case Success(value) => value
-        }
-
-        busy()
-
-    }
-
-  def busy(): Behavior[Command] =
-    Behaviors.receiveMessage[Command] {
-
-      case cmd: Command.DoRequest =>
-        stashBuffer.stash(cmd)
-        Behaviors.same[Command]
-
-      case Command.DoAnswer(ev, replyTo) =>
-        replyTo ! ev
-        stashBuffer.unstashAll(ready())
-    }
-
-  def doRequest(uri: Uri, replyTo: ActorRef[Event]): Future[Command] =
-    Http().singleRequest(HttpRequest(uri = uri)).flatMap {
-      case response @ HttpResponse(StatusCodes.OK, _, entity, _) =>
-        findTitle(entity = entity).map { title =>
-          Command.DoAnswer(
-            Event.Ok(
-              uri = uri,
-              title = title
-            ),
-            replyTo
-          )
-
-        }
-
-      case response @ HttpResponse(status, _, _, _) =>
-        response.discardEntityBytes()
-        Future.successful {
-          Command.DoAnswer(
-            Event.Err(
-              uri = uri,
-              status = Some(status.intValue()),
-              message = "The http request was unsuccessful"
-            ),
-            replyTo
           )
         }
-
-    }
-
-  def findTitle(entity: ResponseEntity): Future[String] =
-    entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { byteString =>
-      byteString.utf8String match {
-        case Worker.titlePattern(title) =>
-          title
-        case other =>
-          throw new IOException(
-            s"Received reponse doesn't contain title tag[${other.take(100)}...]"
-          )
-      }
-    }
-
-}
-
-object Worker {
-
-  val titlePattern: Regex = "(?i)title>(.+)<.*".r
-
-  sealed trait Command
-
-  object Command {
-    final case class DoRequest(uri: Uri, replyTo: ActorRef[Event])
-        extends Command
-
-    final case class DoAnswer(ev: Event, replyTo: ActorRef[Event])
-        extends Command
-  }
-
-  sealed trait Event
-
-  object Event {
-
-    final case class Err(uri: Uri, status: Option[Int], message: String)
-        extends IOException(message)
-        with Event
-
-    object Err {
-      def general(uri: Uri, message: String): Err =
-        new Err(uri = uri, status = None, message = message)
-    }
-
-    final case class Ok(uri: Uri, title: String) extends Event
-  }
-
-  def apply(bufferSize: Int)(implicit ec: ExecutionContext) =
-    Behaviors.setup[Command] { ctx: ActorContext[Command] =>
-      Behaviors.withStash[Command](bufferSize) { stashBuffer =>
-        new Worker(ctx = ctx, stashBuffer = stashBuffer).ready()
       }
 
-    }
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    println(
+      s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop..."
+    )
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+
 }
---
+++ src/main/scala/app/actor/Worker.scala
@@ -0,0 +1,138 @@
+package app.actor
+
+import org.apache.pekko.actor.typed._
+import org.apache.pekko.actor.typed.scaladsl._
+import org.apache.pekko.http.scaladsl.Http
+import org.apache.pekko.http.scaladsl.model._
+import org.apache.pekko.http.scaladsl.server.Directives._
+import org.apache.pekko.util.ByteString
+
+import java.io.IOException
+import java.net.URI
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.io.StdIn
+import scala.util._
+import scala.util.matching.Regex
+
+import Worker._
+
+class Worker(ctx: ActorContext[Command], stashBuffer: StashBuffer[Command])(
+    implicit ec: ExecutionContext
+) {
+
+  implicit val system = ctx.system
+
+  def ready(): Behavior[Command] =
+    Behaviors.receiveMessagePartial[Command] {
+
+      case Command.DoRequest(uri, replyTo) =>
+        ctx.pipeToSelf(doRequest(uri, replyTo)) {
+          case Failure(exception) =>
+            Command.DoAnswer(
+              Event.Err.general(
+                uri,
+                message = s"An error occurred[${exception.getMessage()}]"
+              ),
+              replyTo
+            )
+          case Success(value) => value
+        }
+
+        busy()
+
+    }
+
+  def busy(): Behavior[Command] =
+    Behaviors.receiveMessage[Command] {
+
+      case cmd: Command.DoRequest =>
+        stashBuffer.stash(cmd)
+        Behaviors.same[Command]
+
+      case Command.DoAnswer(ev, replyTo) =>
+        replyTo ! ev
+        stashBuffer.unstashAll(ready())
+    }
+
+  def doRequest(uri: Uri, replyTo: ActorRef[Event]): Future[Command] =
+    Http().singleRequest(HttpRequest(uri = uri)).flatMap {
+      case response @ HttpResponse(StatusCodes.OK, _, entity, _) =>
+        findTitle(entity = entity).map { title =>
+          Command.DoAnswer(
+            Event.Ok(
+              uri = uri,
+              title = title
+            ),
+            replyTo
+          )
+
+        }
+
+      case response @ HttpResponse(status, _, _, _) =>
+        response.discardEntityBytes()
+        Future.successful {
+          Command.DoAnswer(
+            Event.Err(
+              uri = uri,
+              status = Some(status.intValue()),
+              message = "The http request was unsuccessful"
+            ),
+            replyTo
+          )
+        }
+
+    }
+
+  def findTitle(entity: ResponseEntity): Future[String] =
+    entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { byteString =>
+      byteString.utf8String match {
+        case Worker.titlePattern(title) =>
+          title
+        case other =>
+          throw new IOException(
+            s"Received reponse doesn't contain title tag[${other.take(100)}...]"
+          )
+      }
+    }
+
+}
+
+object Worker {
+
+  val titlePattern: Regex = "(?i)title>(.+)<.*".r
+
+  sealed trait Command
+
+  object Command {
+    final case class DoRequest(uri: Uri, replyTo: ActorRef[Event])
+        extends Command
+
+    final case class DoAnswer(ev: Event, replyTo: ActorRef[Event])
+        extends Command
+  }
+
+  sealed trait Event
+
+  object Event {
+
+    final case class Err(uri: Uri, status: Option[Int], message: String)
+        extends IOException(message)
+        with Event
+
+    object Err {
+      def general(uri: Uri, message: String): Err =
+        new Err(uri = uri, status = None, message = message)
+    }
+
+    final case class Ok(uri: Uri, title: String) extends Event
+  }
+
+  def apply(bufferSize: Int)(implicit ec: ExecutionContext) =
+    Behaviors.setup[Command] { ctx: ActorContext[Command] =>
+      Behaviors.withStash[Command](bufferSize) { stashBuffer =>
+        new Worker(ctx = ctx, stashBuffer = stashBuffer).ready()
+      }
+
+    }
+}
