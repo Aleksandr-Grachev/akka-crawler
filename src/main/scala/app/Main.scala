abd89b5d82a22591e73e9621483fb54ff519eb1a
Alex Grachev <alex.f.grach@gmail.com>
2024-10-30 17:12:30 +0300

    app http stub added

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -15,37 +15,87 @@ import scala.io.StdIn
 import scala.util._
 import scala.util.matching.Regex
 
+import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+import org.apache.pekko.actor.CoordinatedShutdown
+import org.apache.pekko.Done
+import spray.json._
+
 object Main {
 
+  val HTTP_PORT  = 7777
   val STASH_SIZE = 100
+  final case class UrlRequest(urls: List[Uri])
+
+  implicit val uriFormat: RootJsonFormat[Uri] =
+    new RootJsonFormat[Uri] {
+      def read(json: JsValue): Uri =
+        json match {
+
+          case JsString(value) =>
+            try {
+              val ret = Uri.parseAbsolute(org.parboiled2.ParserInput(value))
+              ret
+            } catch {
+              case IllegalUriException(ex) =>
+                deserializationError(
+                  s"The given value[$value] isn't a valid URI"
+                )
+            }
+
+          case other =>
+            deserializationError("An JsString expected with Uri format")
+
+        }
+      def write(obj: Uri): JsValue = JsString(obj.toString())
+    }
+  implicit val urlRequestFormat: RootJsonFormat[UrlRequest] = jsonFormat1(
+    UrlRequest.apply
+  )
 
   def main(args: Array[String]): Unit = {
 
-    implicit val system = ActorSystem(Behaviors.empty, "my-system")
-    // needed for the future flatMap/onComplete in the end
+    implicit val system = ActorSystem(Behaviors.empty, "crawler-system")
+
     implicit val executionContext = system.executionContext
 
     val route =
-      path("hello") {
-        get {
-          complete(
-            HttpEntity(
-              ContentTypes.`text/html(UTF-8)`,
-              "<h1>Say hello to Pekko HTTP</h1>"
+      path("urls") {
+        post {
+          entity(as[UrlRequest]) { urlRequest: UrlRequest =>
+            complete(
+              HttpEntity(
+                ContentTypes.`text/html(UTF-8)`,
+                s"<h1>Say hello to Pekko HTTP[$urlRequest]</h1>"
+              )
             )
-          )
+          }
+
         }
       }
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture =
+      Http().newServerAt("localhost", HTTP_PORT).bind(route).andThen {
+        case Success(value) =>
+          println(
+            s"Server now online. Please navigate to http://localhost:$HTTP_PORT/urls"
+          )
 
-    println(
-      s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop..."
-    )
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+        case Failure(ex) =>
+          println(
+            s"Something get wrong[${ex.getMessage()}]"
+          )
+      }
+
+    CoordinatedShutdown(system).addTask(
+      CoordinatedShutdown.PhaseBeforeServiceUnbind,
+      "info"
+    ) { () =>
+      println("Shuting down crawler")
+      Future.successful(Done)
+    }
+
   }
 
 }
