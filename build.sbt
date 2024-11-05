3d9b79a3eb140a1f7048edfff95149e713e07206
Alex Grachev <alex.f.grach@gmail.com>
2024-11-05 14:56:03 +0300

    logback, testkit, mock-server dependencies added all deps moved to separate file

--- build.sbt
+++ build.sbt
@@ -1,23 +1,19 @@
+import Dependency._
+
 version := "1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.15"
 
+//main
+libraryDependencies ++= pekko.pekkoTypedDeps
+libraryDependencies ++= loggingDeps
+//test
+libraryDependencies ++= mockServerDeps
+libraryDependencies ++= scalaTestDeps
+libraryDependencies ++= pekko.pekkoTestKitDeps
 
-lazy val pekkoDeps = {
-  val V = "1.1.0"
-  Seq(
-    "org.apache.pekko" %% "pekko-stream"          % V,
-    "org.apache.pekko" %% "pekko-stream-typed"    % V,
-    "org.apache.pekko" %% "pekko-http"            % V,
-    "org.apache.pekko" %% "pekko-http-spray-json" % V,
-    "org.apache.pekko" %% "pekko-actor-typed"     % V
-  )
-}
-
-libraryDependencies ++= pekkoDeps
-
-scalacOptions ++= Seq( // use ++= to add to existing options
+scalacOptions ++= Seq(
   "-encoding",
-  "utf8", // if an option takes an arg, supply it on the same line
+  "utf8",
   "-Wunused"
 )
---
+++ project/Dependency.scala
@@ -0,0 +1,49 @@
+import sbt._
+import sbt.librarymanagement.DependencyBuilders
+
+object Dependency {
+
+  object pekko {
+    val V = "1.1.0"
+
+    val pekkoTypedDeps =
+      Seq(
+        "org.apache.pekko" %% "pekko-stream"          % V,
+        "org.apache.pekko" %% "pekko-stream-typed"    % V,
+        "org.apache.pekko" %% "pekko-http"            % V,
+        "org.apache.pekko" %% "pekko-http-spray-json" % V,
+        "org.apache.pekko" %% "pekko-actor-typed"     % V
+      )
+
+    val pekkoTestKitDeps =
+      Seq(
+        // "org.apache.pekko" %% "pekko-stream-testkit" % V % Test,
+        // "org.apache.pekko" %% "pekko-http-testkit"   % V % Test
+        "org.apache.pekko" %% "pekko-actor-testkit-typed" % V % Test
+      )
+  }
+
+  lazy val loggingDeps = {
+    val V = "1.5.12"
+    Seq(
+      "ch.qos.logback" % "logback-classic" % V
+    )
+  }
+
+  lazy val mockServerDeps = {
+    val V = "5.15.0"
+    Seq(
+      "org.mock-server" % "mockserver-netty" % V % Test
+    )
+  }
+
+  lazy val scalaTestDeps = {
+    val V = "3.2.19"
+    Seq(
+      "org.scalactic" %% "scalactic" % V % Test,
+      "org.scalatest" %% "scalatest" % V % Test
+    )
+
+  }
+
+}
